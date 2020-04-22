36b5f483e2a1431302218f4fcba62b17a81afc47
Tomáš Mozdřeň <mozdrent@gmail.com>
2020-04-22 09:55:37 +0200

    minor changes
    cv moved into newly created documents page
    added borders to wallpapers in menu on wallpapers page
    added borders to documents in menu in documents page

--- js/headerScript.js
+++ js/headerScript.js
@@ -3,9 +3,9 @@ document.write(
 		"<div class='container navBar'>" +
 			"<a href='index.html'><i class='fa fa-home fa-fw fa-2x'></i></a>" +
 			"<a href='mailto:mozdrent@gmail.com?Subject=Hi%20Thomas'><i class='fa fa-envelope fa-fw fa-2x'></i></a>" +
-			"<a target='_blank' href='cv/cv.pdf'><i class='fa fa-file-pdf-o fa-fw fa-2x'></i></a>" +
+			"<a href='pages/documents.html'><i class='fa fa-file-pdf-o fa-fw fa-2x'></i></a>" +
+			"<a href='pages/wallpaper.html'><i class='fa fa-picture-o fa-fw fa-2x'></i></a>"+
 			"<a target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
-      "<a target='_blank' href='pages/wallpaper.html'><i class='fa fa-picture-o fa-fw fa-2x'></i></a>"+
 		"</div>" +
 	"</aside>"
 );
