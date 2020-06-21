6fa6b1ea29c4b94793d08cb78308a20341d6701b
Tomáš Mozdřeň <mozdrent@gmail.com>
2020-06-21 07:33:53 +0200

    added icons

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,10 +1,10 @@
 document.write(
     "<div id='topNav' class='navbar'>"+
-        "<a title='Home' href='index.html'>Home</i></a>" +
-        "<a title='Email me!' href='mailto:mozdrent@gmail.com?Subject=Hi%20Thomas'>Email me!</a>" +
-        "<a title='Documents' href='pages/documents.html'>Documents</a>" +
-        "<a title='Wallpapers' href='pages/wallpaper.html'>Wallpapers</a>"+
-        "<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'>Main YT channel</a>"+
-        "<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'>Music YT channel</a>"+
+        "<a title='Home' href='index.html'><img height='25px' width='25px' src='icons/home.png'></img></a>" +
+        "<a title='Email me!' href='mailto:mozdrent@gmail.com?Subject=Hi%20Thomas'><img height='25px' width='25px' src='icons/email.png'></img></a>" +
+        "<a title='Documents' href='pages/documents.html'><img height='25px' width='25px' src='icons/document.png'></img></a>" +
+        "<a title='Wallpapers' href='pages/wallpaper.html'><img height='25px' width='25px' src='icons/picture.png'></img></a>"+
+        "<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><img height='25px' width='25px' src='icons/yt.png'></img></a>"+
+        "<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'><img height='25px' width='25px' src='icons/yt.png'></img></a>"+
     "</div>"
 );
