f961574c1397902a79c77e106e5eb581a234c453
BeanGreen247 <mozdrent@gmail.com>
2020-08-26 13:26:35 +0200

    reworked landing page

---
+++ js/topNavScript.js
@@ -0,0 +1,14 @@
+document.write(
+    "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'></link>"+
+    "<div class='topnav animate-bottom'>"+
+    "<img src='logo.png' alt='logo'></img>"+
+    "<a href='#contact'><i class='fa fa-fw fa-address-book'></i><span>Contact</span></a>"+
+    "<a href='#wallpapers'><i class='fa fa-fw fa-picture-o'></i><span>Wallpapers</span></a>"+
+    "<a href='#software'><i class='fa fa-fw fa-tasks'></i><span>Software</span></a>"+
+    "<a href='#os'><i class='fa fa-fw fa-linux'></i><span>OS</span></a>"+
+    "<a href='#setup'><i class='fa fa-fw fa-desktop'></i><span>Workstation</span></a>"+
+    "<a href='#documents'><i class='fa fa-fw fa-file-text'></i><span>Documents</span></a>"+
+    "<a href='#projects'><i class='fa fa-fw fa-terminal'></i><span>Projects</span></a>"+
+    "<a href='#about'><i class='fa fa-fw fa-home'></i><span>About</span></a>"+
+    "</div>"
+);
