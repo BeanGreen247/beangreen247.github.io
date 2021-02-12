444516938ff0ae82b2de3f04c7b9d19980e6f28c
BeanGreen247 <mozdrent@gmail.com>
2021-02-12 15:01:59 +0100

    website redesign and structure reworked

--- js/topNavScript.js
+++ js/topNavScript.js
@@ -1,13 +1,11 @@
 document.write(
     "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'></link>"+
     "<div class='topnav animate-bottom'>"+
-    "<img src='logo.png' alt='logo'></img>"+
-    "<a href='#contact'><i class='fa fa-fw fa-address-book'></i><span>Contact</span></a>"+
-    "<a href='#wallpapers'><i class='fa fa-fw fa-picture-o'></i><span>Wallpapers</span></a>"+
-    "<a href='#software'><i class='fa fa-fw fa-tasks'></i><span>Software</span></a>"+
-    "<a href='#setup'><i class='fa fa-fw fa-desktop'></i><span>Workstation</span></a>"+
-    "<a href='#documents'><i class='fa fa-fw fa-file-text'></i><span>Documents</span></a>"+
-    "<a href='#projects'><i class='fa fa-fw fa-terminal'></i><span>Projects</span></a>"+
-    "<a href='#about'><i class='fa fa-fw fa-home'></i><span>About</span></a>"+
+    "<a1>Thomas Mozdren</a1>"+
+    "<a href='pages/wallpapers.html'><i class='fa fa-picture-o'></i><span>Wallpapers</span></a>"+
+    "<a href='pages/software.html'><i class='fa fa-fw fa-tasks'></i><span>Software</span></a>"+
+    "<a href='pages/documents.html'><i class='fa fa-fw fa-file-text'></i><span>Documents</span></a>"+
+    "<a href='pages/projects.html'><i class='fa fa-fw fa-terminal'></i><span>Projects</span></a>"+
+    "<a href='index.html'><i class='fa fa-fw fa-home'></i><span>About</span></a>"+
     "</div>"
 );
---
+++ js/topNavScriptPages.js
@@ -0,0 +1,11 @@
+document.write(
+    "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'></link>"+
+    "<div class='topnav animate-bottom'>"+
+    "<a1>Thomas Mozdren</a1>"+
+    "<a href='wallpapers.html'><i class='fa fa-picture-o'></i><span>Wallpapers</span></a>"+
+    "<a href='software.html'><i class='fa fa-fw fa-tasks'></i><span>Software</span></a>"+
+    "<a href='documents.html'><i class='fa fa-fw fa-file-text'></i><span>Documents</span></a>"+
+    "<a href='projects.html'><i class='fa fa-fw fa-terminal'></i><span>Projects</span></a>"+
+    "<a href='../index.html'><i class='fa fa-fw fa-home'></i><span>About</span></a>"+
+    "</div>"
+);
